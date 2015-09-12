acee94e50ce3b26f5e8efd320de81693f29ff47c
Evgeniy Vostrov <evostrov@reg.ru>
2015-09-12 10:34:10 +0400

    init

---
+++ Assets/MyFPSController.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function FixedUpdate () {
+    if ( Input.touchCount > 0 ) {
+        Debug.Log( Input.GetTouch(0).phase );
+    }
+}
+
+function Update () {
+
+}
