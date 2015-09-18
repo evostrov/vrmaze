bd3332a58557107020be260f19a93c4dbd056510
Evgeniy Vostrov <evostrov@reg.ru>
2015-09-18 21:57:31 +0400

    Minor

--- vrmazeserver/maze/views.py
+++ vrmazeserver/maze/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from maze.models import Mazegen, GrowingTree
+import json
 
 def generate_maze(request):
     maze_gen = GrowingTree(size=3)
     maze = maze_gen.generate_maze()
 
-    # Ключи структуры должны быть строками для дампа в json
-    res = {}
+    response_data = {}
     for node in maze:
         str_node = str(node[0]) + ',' + str(node[1])
-        res[str_node] = maze[node]
+        response_data[str_node] = maze[node]
 
-    return JsonResponse(res)
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
