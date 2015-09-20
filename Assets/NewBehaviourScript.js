e725103fbe9ec619cf1faf8f96198a80367a92aa
Evgeniy Vostrov <evostrov@reg.ru>
2015-09-20 17:02:38 +0400

    VR приложение получает структуру лабиринта с сервера и рендерит стены

--- Assets/NewBehaviourScript.js
+++ Assets/NewBehaviourScript.js
@@ -1,13 +1,64 @@
 ﻿#pragma strict
 
+import SimpleJSON;
+
+var LonWallPrefab : GameObject;
+var LatWallPrefab : GameObject;
+
 function Start () {
-    // for (var y = 0; y < 5; y++) {
-    //     for (var x = 0; x < 5; x++) {
-    //         var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-    //         cube.AddComponent.<Rigidbody>();
-    //         cube.transform.position = Vector3 (x, y, 0);
-    //     }
-    // }
+    var url = "http://5-63-157-114.ovz.vps.regruhosting.ru/generate_maze";
+    var www : WWW = new WWW(url);
+
+    yield www;
+
+    if ( www.error == null ) {
+        var answer = JSON.Parse(www.text);
+        Debug.Log(answer);
+
+        var wall_length = LonWallPrefab.transform.localScale.x;
+        var wall_width = LonWallPrefab.transform.localScale.z;
+
+        for ( var x = 0; x < 6; x++ ) {
+            for ( var z = 0; z < 6; z++ ) {
+                var node_x0 = x*wall_length;
+                var node_z0 = z*wall_length;
+
+                var node_directions = answer[x + ',' + z].ToString();
+
+                if ( ! node_directions.Contains("N") ) {
+                    var n_wall = Instantiate(
+                        LatWallPrefab,
+                        new Vector3( node_x0 + wall_width/2, 2, node_z0 + wall_length/2 ),
+                        Quaternion.identity
+                    );
+                }
+                if ( ! node_directions.Contains("S") ) {
+                    var s_wall = Instantiate(
+                        LatWallPrefab,
+                        new Vector3( node_x0 + wall_length - wall_width/2, 2, node_z0 + wall_length/2 ),
+                        Quaternion.identity
+                    );
+                }
+                if ( ! node_directions.Contains("E") ) {
+                    var e_wall = Instantiate(
+                        LonWallPrefab,
+                        new Vector3( node_x0 + wall_length/2, 2, node_z0 + wall_width/2 ),
+                        Quaternion.identity
+                    );
+                }
+                if ( ! node_directions.Contains("W") ) {
+                    var w_wall = Instantiate(
+                        LonWallPrefab,
+                        new Vector3( node_x0 + wall_length/2, 2, node_z0 + wall_length - wall_width/2 ),
+                        Quaternion.identity
+                    );
+                }
+            }
+        }
+    }
+    else {
+        Debug.Log("WWW Error: "+ www.error);
+    }
 }
 
 function Update () {
