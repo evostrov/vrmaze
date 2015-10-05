cf3d3a9907bcd351c04e5720e5daaa29a29630c1
Evgeniy Vostrov <evostrov@reg.ru>
2015-10-05 12:52:10 +0400

    Стрелки и надписи

--- Assets/NewBehaviourScript.js
+++ Assets/NewBehaviourScript.js
@@ -4,6 +4,9 @@ import SimpleJSON;
 
 var LonWallPrefab : GameObject;
 var LatWallPrefab : GameObject;
+var StartFloorPrefab : GameObject;
+var BiDirStartFloorPrefab : GameObject;
+var FinishFloorPrefab : GameObject;
 
 function Start () {
     var url = "http://5-63-157-114.ovz.vps.regruhosting.ru/generate_maze";
@@ -13,7 +16,6 @@ function Start () {
 
     if ( www.error == null ) {
         var answer = JSON.Parse(www.text);
-        // Debug.Log(answer);
 
         var maze_dump = answer['maze_dump'];
         var maze_size = parseInt( answer['size'].ToString().Replace("\"", "") );
@@ -27,6 +29,36 @@ function Start () {
                 var node_z0 = z*wall_length;
 
                 var node_directions = maze_dump[x + ',' + z].ToString();
+
+                if ( x == 0 && z == 0 ) {
+                    var start_floor_z_rotate = 0;
+                    if ( node_directions.Contains("S") ) { start_floor_z_rotate = 270; }
+
+                    if ( node_directions.Contains("S") && node_directions.Contains("W") ) {
+                        var bi_dir_start_node = Instantiate(
+                            BiDirStartFloorPrefab,
+                            new Vector3(5, 0.1f, 5),
+                            Quaternion.Euler(90, 0, 0)
+                        );
+                    }
+                    else {
+                        var start_node = Instantiate(
+                            StartFloorPrefab,
+                            new Vector3(5, 0.1f, 5),
+                            Quaternion.Euler(90, 0, start_floor_z_rotate)
+                        );
+                    }
+                }
+                else if ( x == maze_size-1 && z == maze_size-1 ) {
+                    var finish_floor_z_rotate = 0;
+                    if ( node_directions.Contains("N") ) { finish_floor_z_rotate = 270; }
+
+                    var finish_node = Instantiate(
+                        FinishFloorPrefab,
+                        new Vector3( node_x0 + wall_length/2, 0.1f, node_z0 + wall_length/2 ),
+                        Quaternion.Euler(90, 0, finish_floor_z_rotate)
+                    );
+                }
 
                 if ( ! node_directions.Contains("N") ) {
                     var n_wall = Instantiate(
